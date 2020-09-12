57a9b286ee6cb6bf94cdef9d8af4028d780545ac
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-11 22:31:54 -0500

    Events

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		name: 'Ber',
-		job: 'Web Developer',
-		website: 'http://www.myefficy.com',
-		websiteTag: '<a href="http://www.myefficy.com">myEfficy website </a>',
+		age: 25,
+		x: 0,
+		y: 0,
 	},
 	methods: {
-		greet: function (time) {
-			return 'Good ' + time + ' ' + this.name
+		add: function (inc) {
+			this.age += inc
+		},
+		substract: function (dec) {
+			this.age -= dec
+		},
+		updateXY: function (event) {
+			this.x = event.offsetX
+			this.y = event.offsetY
 		},
 	},
 })
