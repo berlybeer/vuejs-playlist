6571a5e535c171353d91b8f3c8f21fbc13ce4375
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-12 08:55:57 -0500

    Keyboard events

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 new Vue({
 	el: '#vue-app',
-	data: {
-		age: 25,
-		x: 0,
-		y: 0,
-	},
+	data: {},
 	methods: {
-		add: function (inc) {
-			this.age += inc
-		},
-		substract: function (dec) {
-			this.age -= dec
-		},
-		updateXY: function (event) {
-			this.x = event.offsetX
-			this.y = event.offsetY
+		logName: function () {
+			console.log('You entered your name')
 		},
 
-		click: function () {
-			alert('You clicked me')
+		logAge: function () {
+			console.log('You entered your age')
 		},
 	},
 })
