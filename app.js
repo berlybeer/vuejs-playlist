d1f480927f47f1e3aa6b0b39ffd9b94e8ed7f311
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-16 09:46:28 -0500

    Refs

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-Vue.component('greeting', {
-	template:
-		'<p>Hey there, I am a {{name}}. <button v-on:click="changeName">Change name</button></p>',
-	data: function () {
-		return {
-			name: 'Yoshi',
-		}
-	},
-	methods: {
-		changeName: function () {
-			this.name = 'Mario'
-		},
-	},
-})
 new Vue({
 	el: '#vue-app-one',
-})
-
-new Vue({
-	el: '#vue-app-two',
+	data: {
+		output: 'Your fav food',
+		greeting: 'Hello',
+	},
+	methods: {
+		readRefs: function () {
+			this.greeting = this.$refs.input.value
+			this.output = this.$refs.input.value
+		},
+	},
 })
