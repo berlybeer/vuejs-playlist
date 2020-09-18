1affc30671d09b32243755d08efdcdecda5f49c5
berlybeer <b.pumacajia.cz@gmail.com>
2020-09-18 13:14:14 -0500

    GET Requests

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <add-blog></add-blog>
+    <show-blog></show-blog>
   </div>
 </template>
 
 <script>
 import addBlog from "./components/addBlog.vue";
+import showBlogs from "./components/showBlogs.vue";
+
 export default {
   components: {
     "add-blog": addBlog,
+    "show-blog": showBlogs
   },
   data() {
     return {};
   },
-  methods: {},
+  methods: {}
 };
 </script>
 
